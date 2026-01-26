780da599e00d758535e8f82b5ee4807726ed65e1
DEV.JS <pztdrd@nate.com>
2026-01-26 15:16:50 +0900

    대소문 구분

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../Models/user");
 const { generateSalt, sha256HashPassword } = require("../utils/password");
 
 router.post("/users", async (req, res) => {
